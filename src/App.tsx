b840b678d4efe05c91461ca93bbda68d60a89a17
LiuJB0128 <1178570567@qq.com>
2021-10-30 20:22:44 +0800

    创建 Views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import Tags from 'views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -27,36 +29,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在！</div>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
   );
 }
 
