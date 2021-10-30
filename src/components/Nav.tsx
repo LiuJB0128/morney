90f4d184f11805651e382d2b6d677a378b8be5d5
LiuJB0128 <1178570567@qq.com>
2021-10-30 21:20:35 +0800

    修复 Nav 点击区域过小

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,43 +5,55 @@ import Icon from './Icon';
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
-    display:flex;
+    display: flex;
+
     > li {
       width: 33.333333%;
-      text-align:center;
-      padding: 4px 0;
-      display:flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      .icon {
-        width: 24px;
-        height: 24px;
+      text-align: center;
+
+      > a {
+        display: flex;
+        padding: 4px 0;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
-`
+`;
 const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="statistics"/>
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="statistics"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 export default Nav;
