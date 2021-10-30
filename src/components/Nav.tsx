9acfa5751d6134f1b7b50e29ca06ba04b11ccc1b
LiuJB0128 <1178570567@qq.com>
2021-10-30 22:36:05 +0800

    当前路由自动高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -25,6 +25,14 @@ const NavWrapper = styled.nav`
           width: 24px;
           height: 24px;
         }
+
+        &.selected {
+          color: #f40;
+
+          .icon {
+            fill: #f40;
+          }
+        }
       }
     }
   }
@@ -34,22 +42,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags" activeClassName="selected">
             <Icon name="tag"/>
             标签页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name="money"/>
             记账页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="statistics"/>
             统计页
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
