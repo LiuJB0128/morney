aacd75e9094286618e15b309d312c873c443a893
LiuJB0128 <1178570567@qq.com>
2021-11-01 22:56:34 +0800

    Money.tsx 类型 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,29 @@ import Layout from 'components/Layout';
 import React from 'react';
 import styled from 'styled-components';
 
-const CategorySection = styled.section``
+const CategorySection = styled.section`
+  font-size: 16px;
+  > ul {
+    display:flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after {
+        content: '';
+        display:block;
+        background: #333;
+        height: 3px;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
+`
 
 const InputSection = styled.section``
 
@@ -55,7 +77,7 @@ function Money() {
     <Layout>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
