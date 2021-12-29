866857074ff181f996fe565aa5b4e9dd3abab0bd
LiuJB0128 <1178570567@qq.com>
2021-12-29 17:00:50 +0800

    创建 createId 让 id 自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
+import {createId} from './lib/createId';
 
 const useTags = () => { //封装一个自定义 Hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    {id: 1, name: '其他'},
-    {id: 2, name: '餐饮'},
-    {id: 3, name: '交通'},
-    {id: 4, name: '购物'}
+    {id: createId(), name: '其他'},
+    {id: createId(), name: '餐饮'},
+    {id: createId(), name: '交通'},
+    {id: createId(), name: '购物'}
   ]);
   return {tags, setTags};
 };
