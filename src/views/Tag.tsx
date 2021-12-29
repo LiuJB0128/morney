9ebb1cbb575fdd660b223c858c01ca19ffec1d71
LiuJB0128 <1178570567@qq.com>
2021-12-29 19:43:13 +0800

    Tag.tsx 的样式完成

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,6 +5,9 @@ import {Layout} from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
 import styled from 'styled-components';
+import { Input } from 'components/Input';
+import {Center} from 'components/Center';
+import { Space } from 'components/Space';
 
 type Params = {
   id: string
@@ -17,6 +20,11 @@ const Topbar = styled.header`
   padding: 14px;
   background: #FF9E00;
 `;
+const InputWrapper = styled.div`
+background: white;
+  padding: 0 16px;
+  margin-top: 8px;
+`
 const Tag: React.FC = () => {
   const {findTag} = useTags();
   let {id} = useParams<Params>();
@@ -28,13 +36,15 @@ const Tag: React.FC = () => {
         <span>编辑分类</span>
         <Icon/>
       </Topbar>
-      <div>
-        <span>名称</span>
-        <input type="text" placeholder={tag.name}/>
-      </div>
-      <div>
+      <InputWrapper>
+        <Input label="名称" type="text" placeholder={tag.name}/>
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
         <Button>删除分类</Button>
-      </div>
+      </Center>
     </Layout>
   );
 };
