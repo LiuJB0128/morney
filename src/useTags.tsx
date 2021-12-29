338086cd82dd3a2abb86f71b69af08d9059109ea
LiuJB0128 <1178570567@qq.com>
2021-12-29 16:46:44 +0800

    重构：将tag:string 重构为 tag:{id:number;name:string}

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,12 @@
 import {useState} from 'react';
 
 const useTags = () => { //封装一个自定义 Hook
-  const [tags, setTags] = useState<string[]>(['其他', '餐饮', '交通', '购物']);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>([
+    {id: 1, name: '其他'},
+    {id: 2, name: '餐饮'},
+    {id: 3, name: '交通'},
+    {id: 4, name: '购物'}
+  ]);
   return {tags, setTags};
 };
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -15,7 +15,7 @@ type Category = '-' | '+'
 
 function Money() {
   const [selected, setSelected] = useState({
-    tags: [] as string[],
+    tagIds: [] as number[],
     note: '',
     category: '-' as Category,
     amount: '￥0'
@@ -27,8 +27,8 @@ function Money() {
     <MyLayout>
       <CategorySection value={selected.category}
                        onChange={(category) => onChange({category})}/>
-      <TagsSection value={selected.tags}
-                   onChange={(tags) => onChange({tags})}/>
+      <TagsSection value={selected.tagIds}
+                   onChange={(tagIds) => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={(note) => onChange({note})}/>
       <NumberPadSection value={selected.amount}
